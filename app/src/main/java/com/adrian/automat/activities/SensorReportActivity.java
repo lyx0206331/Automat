b3f453548879457773a9fbe326137a659bbdd5d5
ranqing <rq880113@163.com>
2017-07-07 23:19:23 +0800

    add machine info

--- app/src/main/java/com/adrian/automat/activities/SensorReportActivity.java
+++ app/src/main/java/com/adrian/automat/activities/SensorReportActivity.java
@@ -4,8 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.adrian.automat.R;
+import com.adrian.automat.tools.CommUtil;
+import com.adrian.automat.tools.HttpListener;
+import com.adrian.automat.tools.NetUtil;
+import com.yanzhenjie.nohttp.rest.Response;
 
-public class SensorReportActivity extends BaseActivity {
+public class SensorReportActivity extends BaseActivity implements HttpListener {
+
+    private NetUtil util;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +20,7 @@ public class SensorReportActivity extends BaseActivity {
 
     @Override
     protected void initVariables() {
-
+        util = new NetUtil(this, this);
     }
 
     @Override
@@ -24,11 +30,22 @@ public class SensorReportActivity extends BaseActivity {
 
     @Override
     protected void loadData() {
-
+        util.getMachineInfo();
     }
 
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_sensor_report;
+    }
+
+    @Override
+    public void onSucceed(int what, Response response) {
+        String respStr = response.get().toString();
+        CommUtil.logE("MAC_INFO", respStr);
+    }
+
+    @Override
+    public void onFailed(int what, Response response) {
+
     }
 }
